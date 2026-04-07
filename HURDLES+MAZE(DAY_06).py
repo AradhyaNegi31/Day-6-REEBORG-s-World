9bb6671562cb8b33fa3134026dd5cc86ab4d5ab3
Aradhya31 <aradhyanegi3105@gmail.com>
2026-04-07 19:46:07 +0530

    Add files via upload

---
+++ HURDLES+MAZE(DAY_06).py
@@ -0,0 +1,105 @@
+'''  REEBORG's World  '''
+
+# HURDLE 1 
+
+# def move_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+# def solve():
+#     move()
+#     turn_left()
+#     move()
+#     move_right()
+#     move_right()
+#     turn_left()
+# i=1
+# while i<=6:
+#     solve()
+#     i+=1
+
+
+
+# HURDLE 3 
+
+# def move_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while True:
+#     if at_goal():
+#         break
+#     while front_is_clear() and not(right_is_clear()):
+#         if at_goal():
+#             break
+#         move()
+#     else:
+#         turn_left()
+#         move()
+#         move_right()
+#         move()
+#         move_right()
+#         move()
+#         turn_left()
+#         while front_is_clear() and not(right_is_clear()):
+#             if at_goal:
+#                 break
+#             move()
+
+
+
+# HURDLE 4
+
+# def move_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while True:
+#     if at_goal():
+#         break
+#     while front_is_clear() and not(right_is_clear()):
+#         if at_goal():
+#             break
+#         move()
+#     else:
+#         turn_left()
+#         while wall_on_right():
+#             move()
+#         move_right()
+#         move()
+#         move_right()
+#         while front_is_clear():
+#             move()
+#         turn_left()
+#         while front_is_clear() and not(right_is_clear()):
+#             if at_goal:
+#                 break
+#             move()
+
+
+
+# MAZE
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+
+            
+            
+        
+
+
+            
+            
+        
